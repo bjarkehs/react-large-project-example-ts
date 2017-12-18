d13a0d8e7b09af3cfbbd039a9f181bbc338962b5
bjerh <jakob@letsfly.dk>
2017-12-18 10:13:23 +0100

    Static data example

--- src/scenes/UsersOverview/components/UserList/index.tsx
+++ src/scenes/UsersOverview/components/UserList/index.tsx
@@ -1,16 +1,36 @@
 import * as React from 'react';
 
+interface Person {
+  name: string;
+}
+
 class UserList extends React.Component {
 
-	render() {
-		return (
-			<div>
-				<h3>
-					User list
-				</h3>
-			</div>
-		);
-	}
-};
+  usersData: Person[] = [
+    { name: 'Jakob' },
+    { name: 'Bjarke' }
+  ];
+
+  users() {
+    return this.usersData.map((user: Person) =>
+      (
+        <div key={user.name}>
+          {user.name}
+        </div>
+      )
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>
+          User list
+        </h3>
+        {this.users()}
+      </div>
+    );
+  }
+}
 
 export default UserList;
