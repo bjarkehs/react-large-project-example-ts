ea8c069dc1d7bc01e81ffffd03b29fba5f49bf25
Bjarke Hesthaven Nie Søndergaard <bhsgaard@gmail.com>
2017-12-18 10:06:03 +0100

    Minor style stuff

--- src/scenes/UsersOverview/components/UserList/index.tsx
+++ src/scenes/UsersOverview/components/UserList/index.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 
 class UserList extends React.Component {
-
-	render() {
-		return (
-			<div>
-				<h3>
-					User list
-				</h3>
-			</div>
-		);
-	}
-};
+  render() {
+    return (
+      <div>
+        <h3>User list</h3>
+      </div>
+    );
+  }
+}
 
 export default UserList;
