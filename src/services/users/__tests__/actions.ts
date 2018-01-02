2491a1acfb532aba0ee2379ecee87bb8886bb43c
Jakob Bjerre Petersen <Y08E@nykredit.dk>
2018-01-02 10:54:57 +0100

    Uncommented actions.ts content

--- src/services/users/__tests__/actions.ts
+++ src/services/users/__tests__/actions.ts
@@ -1,4 +1,4 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+// import configureMockStore from 'redux-mock-store';
+// import thunk from 'redux-thunk';
 
-import fetchMock from 'fetch-mock';
+// import fetchMock from 'fetch-mock';
