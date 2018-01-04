f503d53c244b1b6192ab0cca07bc54646b169c0d
Jakob Bjerre Petersen <Y08E@nykredit.dk>
2018-01-04 14:18:24 +0100

    Added typings images

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,9 @@
 declare module 'react-router-test-context';
+
+// Images
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.webp';
